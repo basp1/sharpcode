4a818d862f65fb6563ead2a815a3df6f00aa040c
basp <akovn@yandex.ru>
2019-07-10 13:24:57 +0300

    Add SegmentTree

---
+++ src/SegmentTree.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace sharpcode
+{
+    public class SegmentTree<T>
+    {
+        int capacity;
+        T limit;
+        Func<T, T, T> selectFunc;
+        List<T> values;
+
+        public SegmentTree(T limit, Func<T, T, T> selectFunc, IList<T> values) :
+        this(limit, selectFunc, values.Count)
+        {
+            for (int i = 0; i < values.Count; i++)
+            {
+                this.values[capacity - 1 + i] = values[i];
+            }
+
+            for (int i = capacity - 2; i >= 0; i--)
+            {
+                this.values[i] = selectFunc(this.values[1 + (i << 1)], this.values[2 + (i << 1)]);
+            }
+        }
+
+        public SegmentTree(T limit, Func<T, T, T> selectFunc, int dim)
+        {
+            this.limit = limit;
+            this.selectFunc = selectFunc;
+
+            capacity = 1 << (int)Math.Ceiling(Math.Log(dim, 2));
+            int cap = capacity << 1;
+
+            values = new List<T>(cap);
+
+            for (int i = 0; i < cap; i++)
+            {
+                values.Add(limit);
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+
+        public bool Equals(SegmentTree<T> that)
+        {
+            if (capacity != that.Capacity)
+            {
+                return false;
+            }
+
+            if (!values.SequenceEqual(that.values))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return Range(index, index);
+            }
+
+            set
+            {
+                SetValue(index, value);
+            }
+        }
+
+        void SetValue(int index, T value)
+        {
+            if (index >= capacity)
+            {
+                throw new ArgumentOutOfRangeException("'index' should be < 'capacity'");
+            }
+
+            index += (capacity - 1);
+            values[index] = value;
+
+            while (index > 0)
+            {
+                T val = values[index];
+                int neighbor;
+                if (1 == index % 2)
+                {
+                    neighbor = index + 1;
+                    index = index >> 1;
+                }
+                else
+                {
+                    neighbor = index - 1;
+                    index = (index >> 1) - 1;
+                }
+
+                value = selectFunc(val, values[neighbor]);
+                if (!value.Equals(values[index]))
+                {
+                    values[index] = value;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public T Top
+        {
+            get
+            {
+                return values[0];
+            }
+        }
+
+        public T Range(int left, int right)
+        {
+            if (left > right)
+            {
+                throw new ArgumentOutOfRangeException("'left' should be <= 'right'");
+            }
+            if (left >= capacity)
+            {
+                throw new ArgumentOutOfRangeException("'left' should be < 'capacity'");
+            }
+            if (right >= capacity)
+            {
+                throw new ArgumentOutOfRangeException("'right' should be < 'capacity'");
+            }
+
+            left += (capacity - 1);
+            right += (capacity - 1);
+            T leftValue = limit;
+            T rightValue = limit;
+
+            while (left < right)
+            {
+                if (0 == left % 2)
+                {
+                    leftValue = selectFunc(leftValue, values[left]);
+                }
+                left = left >> 1;
+
+                if (1 == right % 2)
+                {
+                    rightValue = selectFunc(values[right], rightValue);
+                }
+                right = (right >> 1) - 1;
+            }
+
+            if (left == right)
+            {
+                leftValue = selectFunc(leftValue, values[left]);
+            }
+
+            return selectFunc(leftValue, rightValue);
+        }
+    }
+}
---
+++ test/SegmentTreeTest.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using sharpcode;
+
+namespace sharpcode.test
+{
+    [TestClass]
+    public class SegmentTreeTest
+    {
+        [TestMethod]
+        public void Range_1()
+        {
+            var u = new SegmentTree<int>(Int32.MinValue, Math.Max, new int[] { 1 });
+
+            Assert.AreEqual(1, u.Top);
+            Assert.AreEqual(1, u.Range(0, 0));
+            Assert.AreEqual(1, u[0]);
+        }
+
+        [TestMethod]
+        public void Range_2()
+        {
+            var u = new SegmentTree<int>(Int32.MinValue, Math.Max, new int[] { 3, 8, 6, 4, 2, 5, 9, 0, 7, 1 });
+
+            Assert.AreEqual(9, u.Top);
+            Assert.AreEqual(9, u.Range(0, 9));
+            Assert.AreEqual(9, u.Range(5, 9));
+            Assert.AreEqual(8, u.Range(0, 4));
+            Assert.AreEqual(8, u.Range(1, 1));
+            Assert.AreEqual(8, u.Range(1, 4));
+            Assert.AreEqual(7, u.Range(7, 8));
+            Assert.AreEqual(5, u.Range(4, 5));
+            Assert.AreEqual(6, u.Range(2, 5));
+        }
+
+        [TestMethod]
+        public void Range_3()
+        {
+            var u = new SegmentTree<int>(Int32.MinValue, Math.Max, new int[] { 5, 4, 3, 2, 1 });
+
+            Assert.AreEqual(5, u.Top);
+            Assert.AreEqual(3, u.Range(2, 4));
+            Assert.AreEqual(5, u[0]);
+            Assert.AreEqual(4, u[1]);
+            Assert.AreEqual(3, u[2]);
+        }
+
+        [TestMethod]
+        public void Set_1()
+        {
+            var values = new int[] { 3, 8, 6, 4, 2, 5, 9, 0, 7, 1 };
+            var u = new SegmentTree<int>(Int32.MinValue, Math.Max, values);
+
+            var v = new SegmentTree<int>(Int32.MinValue, Math.Max, dim: 10);
+
+            Assert.IsFalse(u.Equals(v));
+
+            for (int i = 0; i < 10; i++)
+            {
+                v[i] = values[i];
+            }
+
+            Assert.IsTrue(u.Equals(v));
+        }
+
+        [TestMethod]
+        public void Set_2()
+        {
+            var u = new SegmentTree<int>(Int32.MinValue, Math.Max, new int[] { 3, 8, 6, 4, 2, 5, 9, 0, 7, 1 });
+
+            u[6] = 0;
+
+            Assert.AreEqual(8, u.Top);
+            Assert.AreEqual(8, u.Range(0, 9));
+            Assert.AreEqual(7, u.Range(5, 9));
+            Assert.AreEqual(8, u.Range(0, 4));
+            Assert.AreEqual(8, u[1]);
+            Assert.AreEqual(8, u.Range(1, 4));
+            Assert.AreEqual(7, u.Range(7, 8));
+            Assert.AreEqual(5, u.Range(4, 5));
+            Assert.AreEqual(6, u.Range(2, 5));
+        }
+    }
+}
