c954c8ade4c6b6bdf1867f7751e2b539b38fdfa8
basp <akovn@yandex.ru>
2019-07-10 14:31:09 +0300

    Add FlatMap

---
+++ src/FlatMap.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharpcode
+{
+    public class FlatMap<Key, Value>
+    {
+        List<Key> keys;
+        List<Value> values;
+        Func<Key, Key, int> compareFunc;
+
+        public FlatMap(Func<Key, Key, int> compareFunc)
+        {
+            this.compareFunc = compareFunc;
+            keys = new List<Key>();
+            values = new List<Value>();
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return 0 == keys.Count;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return keys.Count;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return keys.Capacity;
+            }
+        }
+
+        public void Reserve(int capacity)
+        {
+            keys.Capacity = capacity;
+            values.Capacity = capacity;
+        }
+
+        public void Clear()
+        {
+            keys.Clear();
+            values.Clear();
+        }
+
+        public bool Contains(Key key)
+        {
+            Value value = default;
+            return Find(key, ref value);
+        }
+
+        public Value Get(Key key)
+        {
+            Value value = default;
+
+            if (!Find(key, ref value))
+            {
+                throw new KeyNotFoundException("'key' not found");
+            }
+
+            return value;
+        }
+
+        public void Insert(Key key, Value value)
+        {
+            int index = LowerBound(key);
+
+            if (index >= 0 && index < Count && 0 == compareFunc(key, keys[index]))
+            {
+                keys[index] = key;
+                values[index] = value;
+            }
+            else
+            {
+                keys.Insert(index, key);
+                values.Insert(index, value);
+            }
+        }
+
+        public void Remove(Key key)
+        {
+            int index = LowerBound(key);
+
+            if (index >= 0 && index < Count && 0 == compareFunc(key, keys[index]))
+            {
+                keys.RemoveAt(index);
+                values.RemoveAt(index);
+            }
+        }
+
+        public Value this[Key key]
+        {
+            get
+            {
+                return Get(key);
+            }
+
+            set
+            {
+                Insert(key, value);
+            }
+        }
+
+        bool Find(Key key, ref Value value)
+        {
+            int index = LowerBound(key);
+
+            if (index >= 0 && index < Count && 0 == compareFunc(key, keys[index]))
+            {
+                value = values[index];
+                return true;
+            }
+
+            return false;
+        }
+
+        int LowerBound(Key key)
+        {
+            int n = keys.Count - 1;
+            int l = 0;
+            int r = n;
+
+            while (l <= r)
+            {
+                int m = l + (r - l) / 2;
+                int cmp = compareFunc(key, keys[m]);
+                if (0 == cmp)
+                {
+                    return m;
+                }
+                if (cmp < 0)
+                {
+                    r = m - 1;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+
+            return l;
+        }
+    }
+}
---
+++ test/FlatMapTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using sharpcode;
+
+namespace sharpcode.test
+{
+    [TestClass]
+    public class FlatMapTest
+    {
+        [TestMethod]
+        public void Insert_1()
+        {
+            var map = new FlatMap<int, char>((a, b) => a - b);
+
+            map[1] = 'a';
+            map[5] = 'e';
+            map[3] = 'c';
+
+            Assert.AreEqual(3, map.Count);
+            Assert.AreEqual('a', map[1]);
+            Assert.AreEqual('c', map[3]);
+            Assert.AreEqual('e', map[5]);
+
+            map[2] = 'b';
+            map[4] = 'd';
+            Assert.AreEqual(5, map.Count);
+            Assert.AreEqual('a', map[1]);
+            Assert.AreEqual('b', map[2]);
+            Assert.AreEqual('c', map[3]);
+            Assert.AreEqual('d', map[4]);
+            Assert.AreEqual('e', map[5]);
+
+            map[4] = 'D';
+            Assert.AreEqual(5, map.Count);
+            Assert.AreEqual('a', map[1]);
+            Assert.AreEqual('b', map[2]);
+            Assert.AreEqual('c', map[3]);
+            Assert.AreEqual('D', map[4]);
+            Assert.AreEqual('e', map[5]);
+        }
+
+        [TestMethod]
+        public void Remove_1()
+        {
+            var map = new FlatMap<int, char>((a, b) => a - b);
+
+            map[1] = 'a';
+            map[5] = 'e';
+            map[3] = 'c';
+            map[2] = 'b';
+            map[4] = 'd';
+
+            map.Remove(3);
+            Assert.AreEqual(4, map.Count);
+            Assert.AreEqual('a', map[1]);
+            Assert.AreEqual('b', map[2]);
+            Assert.AreEqual('d', map[4]);
+            Assert.AreEqual('e', map[5]);
+
+            map.Remove(1);
+            map.Remove(4);
+            map.Remove(5);
+            Assert.AreEqual(1, map.Count);
+            Assert.AreEqual('b', map[2]);
+        }
+    }
+}
