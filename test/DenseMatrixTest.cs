dae6feee35b5b71bc61c4f424b487e480adfd532
basp <akovn@yandex.ru>
2019-07-12 14:15:55 +0300

    Add tests on DenseMatrix

---
+++ test/DenseMatrixTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using sharpcode;
+
+namespace sharpcode.test
+{
+    [TestClass]
+    public class DenseMatrixTest
+    {
+        [TestMethod]
+        public void Set()
+        {
+            var A = new DenseMatrix<double>(3, 3);
+            A[0, 0] = 0; A[0, 1] = 1; A[0, 2] = 1;
+            A[1, 0] = 2; A[1, 1] = 4; A[1, 2] = -2;
+            A[2, 0] = 0; A[2, 1] = 3; A[2, 2] = 15;
+
+            Assert.AreEqual(3, A.Rows);
+            Assert.AreEqual(3, A.Columns);
+
+            Assert.AreEqual(0, A[0, 0]);
+            Assert.AreEqual(1, A[0, 1]);
+            Assert.AreEqual(1, A[0, 2]);
+
+            Assert.AreEqual(2, A[1, 0]);
+            Assert.AreEqual(4, A[1, 1]);
+            Assert.AreEqual(-2, A[1, 2]);
+
+            Assert.AreEqual(0, A[2, 0]);
+            Assert.AreEqual(3, A[2, 1]);
+            Assert.AreEqual(15, A[2, 2]);
+        }
+
+        [TestMethod]
+        public void Set_2()
+        {
+            var A = new DenseMatrix<double>(3, 2);
+            A[0, 0] = 0; A[0, 1] = 1;
+            A[1, 0] = 2; A[1, 1] = 4;
+            A[2, 0] = 0; A[2, 1] = 3;
+
+            Assert.AreEqual(3, A.Rows);
+            Assert.AreEqual(2, A.Columns);
+
+            Assert.AreEqual(0, A[0, 0]);
+            Assert.AreEqual(1, A[0, 1]);
+
+            Assert.AreEqual(2, A[1, 0]);
+            Assert.AreEqual(4, A[1, 1]);
+
+            Assert.AreEqual(0, A[2, 0]);
+            Assert.AreEqual(3, A[2, 1]);
+        }
+
+        [TestMethod]
+        public void Transpose()
+        {
+            var A = new DenseMatrix<double>(3, 2);
+            A[0, 0] = 0; A[0, 1] = 1;
+            A[1, 0] = 2; A[1, 1] = 4;
+            A[2, 0] = 0; A[2, 1] = 3;
+
+            var At = A.Transpose();
+            var Att = At.Transpose();
+            var Attt = Att.Transpose();
+
+            Assert.IsTrue(A.Equals(Att));
+            Assert.IsTrue(At.Equals(Attt));
+            Assert.IsFalse(A.Equals(At));
+        }
+    }
+}
